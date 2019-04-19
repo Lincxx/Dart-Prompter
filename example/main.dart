95fec63309f0f65c45c0e1e35fae91a32ba0a2c5
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-04-19 16:59:59 -0400

    Added in a clear terminal

--- example/main.dart
+++ example/main.dart
@@ -2,5 +2,7 @@ import 'package:prompter_lincxx/src/terminal.dart';
 
 void main() {
   var terminal = new Terminal();
+  terminal.clearScreen();
   terminal.printPrompt("this is really neat");
+  
 }
